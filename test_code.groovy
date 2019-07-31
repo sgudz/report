f8e14015f5d5c0e11e3cad072509c06a400b84bc
sgudz <vortexuelpm@gmail.com>
2019-07-31 21:18:20 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -3,7 +3,7 @@ def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
 
 node () {
   for (element in reports_map) {
-      String[] file = ${element.value}.split("/")
+      String[] file = element.value.split("/")
       String file_name = file[file.lenght-1]
       echo "${file_name}"
       echo "${element.key} ${element.value}"
