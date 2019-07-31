5fe6f3854ea99400bd754cc9c5fdde03b68036a0
sgudz <vortexuelpm@gmail.com>
2019-07-31 21:51:01 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -9,6 +9,34 @@ node () {
       String file_name = element.value.substring(index +1);
       echo "${file_name}"
       echo "${element.key} ${element.value}"
-      // sh ("wget -O ${workspace}/${file_name} ${element.value}")
+      run_cmd("wget -O ${workspace}/${file_name} ${element.value}")
   }
+}
+def run_cmd(String cmd, Boolean returnStdout=false) {
+    def common = new com.mirantis.mk.Common()
+    common.printMsg("Run shell command:\n" + cmd, "blue")
+    def VENV_PATH='/home/jenkins/fuel-devops30'
+    def stderr_path = "/tmp/${JOB_NAME}_${BUILD_NUMBER}_stderr.log"
+    def script = """#!/bin/bash
+        set +x
+        echo 'activate python virtualenv ${VENV_PATH}'
+        . ${VENV_PATH}/bin/activate
+        bash -c -e -x '${cmd.stripIndent()}' 2>${stderr_path}
+    """
+    try {
+        def stdout = sh(script: script, returnStdout: returnStdout)
+        def stderr = readFile("${stderr_path}")
+        def error_message = "\n<<<<<< STDERR: >>>>>>\n" + stderr
+        common.printMsg(error_message, "yellow")
+        common.printMsg("", "reset")
+        return stdout
+    } catch (e) {
+        def stderr = readFile("${stderr_path}")
+        def error_message = e.message + "\n<<<<<< STDERR: >>>>>>\n" + stderr
+        common.printMsg(error_message, "red")
+        common.printMsg("", "reset")
+        throw new Exception(error_message)
+    } finally {
+        sh(script: "rm ${stderr_path} || true")
+    }
 }
