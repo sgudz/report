15fe550053c51ad2fa4039acb8e20443420d3502
sgudz <vortexuelpm@gmail.com>
2019-07-31 21:29:38 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -3,10 +3,13 @@ def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
 
 node () {
   for (element in reports_map) {
-      String[] file = element.value.split("/")
-      String file_name = file[file.lenght - 1]
+      // String[] file = element.value.split("/")
+      // String file_name = file[file.lenght - 1]
+      int index = element.value.lastIndexOf('/');
+      String file_name = element.value.substring(index +1);
       echo "${file_name}"
       echo "${element.key} ${element.value}"
       //wget -O "${workspace}/${filename}" "${element.value}"
   }
 }
+lastone = last(one.split("-"));
