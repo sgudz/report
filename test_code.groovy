cec7d077e1c4a5c1ba2edf5cd388525c942ae711
sgudz <vortexuelpm@gmail.com>
2019-07-31 21:10:05 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -1,8 +1,10 @@
 def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
-                   "kubeconfig": env.KUBECONFIG, "env_name": env.ENV_NAME,
-                   "kaas_version": env.KAAS_VERSION]
+                   "kubeconfig": env.KUBECONFIG, "management_logs": env.MANAGEMENT_LOGS]
 
 for (element in reports_map) {
+    String[] file = ${element.value}.split("/")
+    String file_name = file[file.lenght-1]
+    echo "${file_name}"
     echo "${element.key} ${element.value}"
-    wget -O "${workspace}/${element.value}" 
+    wget -O "${workspace}/${filename}" ${element.value}
 }
