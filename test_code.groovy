f507e09169400aca9e7895a4a3548ab5ae0e8e57
sgudz <vortexuelpm@gmail.com>
2019-07-31 21:12:05 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -1,10 +1,12 @@
 def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
                    "kubeconfig": env.KUBECONFIG, "management_logs": env.MANAGEMENT_LOGS]
 
-for (element in reports_map) {
-    String[] file = ${element.value}.split("/")
-    String file_name = file[file.lenght-1]
-    echo "${file_name}"
-    echo "${element.key} ${element.value}"
-    wget -O "${workspace}/${filename}" ${element.value}
+node () {
+  for (element in reports_map) {
+      String[] file = ${element.value}.split("/")
+      String file_name = file[file.lenght-1]
+      echo "${file_name}"
+      echo "${element.key} ${element.value}"
+      wget -O ${workspace}/${filename} ${element.value}
+  }
 }
