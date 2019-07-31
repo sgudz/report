66b4b7320585abd90591ec90b441ad93e42daaa9
sgudz <vortexuelpm@gmail.com>
2019-07-31 20:56:41 +0300

    Update test_code.groovy

--- test_code.groovy
+++ test_code.groovy
@@ -1,4 +1,7 @@
-def map = [Io: 1, Europa: 2, Ganymed: 3]
+def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
+                   "kubeconfig": env.KUBECONFIG, "env_name": env.ENV_NAME,
+                   "kaas_version": env.KAAS_VERSION]
+
 for (element in map) {
     echo "${element.key} ${element.value}"
 }
