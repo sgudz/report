345be944f3feb910eb49ed8ce9f7d963f7943eae
sgudz <vortexuelpm@gmail.com>
2019-08-01 12:33:46 +0300

    Update kaas_testrail_report.groovy

--- kaas_testrail_report.groovy
+++ kaas_testrail_report.groovy
@@ -1,148 +1,51 @@
-/**
- *
- *
- * Expected parameters:
- *   ENV_NAME                      Fuel-devops environment name
- *   PARENT_NODE_NAME              Name of the jenkins slave to create the environment
- *   PARENT_WORKSPACE              Path to the workspace of the parent job to use tcp-qa repo
- *   TEMPEST_TEST_SUITE_NAME       Name of tempest suite
- */
-
 def common = new com.mirantis.mk.Common()
-def report_filename = env.REPORT_SI_KAAS_BOOTSTRAP
+def reports_map = ["bootstrap_report": env.BOOTSTRAP_REPORT,
+                   "kubeconfig": env.KUBECONFIG, "management_logs": env.MANAGEMENT_LOGS]
 
-timeout(time: 2, unit: 'HOURS') {
 node () {
-    
-    dir("${PARENT_WORKSPACE}") {
-        def description = ''
-        def exception_message = ''
-        try {
-
-            def report_name = ''
-            def testSuiteName = ''
-            def methodname = ''
-            def testrail_name_template = ''
-            def reporter_extra_options = []
-
-            def reports_urls = [deployment_report_name:REPORT_SI_KAAS_BOOTSTRAP]
-
-            if (report_filename) {
-                stage("tcp-qa cases report") {
-                    testSuiteName = "[MCP_X] integration cases"
-                    methodname = "{methodname}"
-                    testrail_name_template = "{title}"
-                    reporter_extra_options = [
-                      "--testrail-add-missing-cases",
-                      "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
-                      "--testrail-case-section-name \'All\'",
-                    ]
-                    ret = upload_results_to_testrail(report_filename, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
-                    common.printMsg(ret.stdout, "blue")
-                    report_url = ret.stdout.split("\n").each {
-                        if (it.contains("[TestRun URL]")) {
-                            common.printMsg("Found report URL: " + it.trim().split().last(), "blue")
-                            description += "<a href=" + it.trim().split().last() + ">${testSuiteName}</a><br>"
-                        }
-                    }
-                    exception_message += ret.exception
-                }
-            }
-
-
-            // Check if there were any exceptions during reporting
-            if (exception_message) {
-                throw new Exception(exception_message)
-            }
-
-        } catch (e) {
-            common.printMsg("Job is failed", "purple")
-            throw e
-        } finally {
-            // reporting is failed for some reason
-            writeFile(file: "description.txt", text: description, encoding: "UTF-8")
+  stage ("Download reports") {
+      reports_map.each { key, val ->
+        if (val) {
+          file_name = val.substring(val.lastIndexOf('/') +1);
+          common.printMsg("Downloading $file_name report to $workspace/$file_name", "purple")
+          run_cmd("wget -O ${workspace}/${file_name} ${val}")
         }
-    }
-}
-} // timeout
+        else {
+          echo "$key is not set. Skipping"
+        } //if else
+      } // iterate map
+  } //stage
 
-def swarm_testrail_report(String passed_steps, String node_with_reports) {
-        // Run pytest tests
-        def common = new com.mirantis.mk.Common()
-        def tempest_test_suite_name = env.TEMPEST_TEST_SUITE_NAME
-        def test_plan_name_prefix = env.TEST_PLAN_NAME_PREFIX ?: ''
-        def parameters = [
-                string(name: 'ENV_NAME', value: "${ENV_NAME}"),
-                string(name: 'TEMPEST_TEST_SUITE_NAME', value: "${tempest_test_suite_name}"),
-                string(name: 'TEST_PLAN_NAME_PREFIX', value: "${test_plan_name_prefix}"),
+  stage ("Report to testrail") {
+      reports_map.each { key, val ->
+        if (val) {
+          report_name = val.substring(val.lastIndexOf('/') +1)
+          println "Reporting ${report_name}"
+          testSuiteName = "[MCP2.0]Integration automation"
+          methodname = "{methodname}"
+          testrail_name_template = "{title}"
+          reporter_extra_options = [
+            "--testrail-add-missing-cases",
+            "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
+            "--testrail-case-section-name \'All\'",
             ]
-        common.printMsg("Start building job 'swarm-testrail-report' with parameters:", "purple")
-        common.prettyPrint(parameters)
-        build job: 'swarm-testrail-report',
-            parameters: parameters
-}
-
-def upload_results_to_testrail(report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options=[]) {
-  def venvPath = '/home/jenkins/venv_testrail_reporter'
-  def testrailURL = "https://mirantis.testrail.com"
-  def testrailProject = "Mirantis Cloud Platform"
-  def testPlanNamePrefix = env.TEST_PLAN_NAME_PREFIX ?: "[2019.2.0-update]System"
-  def testPlanName = "${testPlanNamePrefix}-${ENV_NAME}-${new Date().format('yyyy-MM-dd')}"
-  def testPlanDesc = env.ENV_NAME
-  def testrailMilestone = "MCP1.1"
-  def testrailCaseMaxNameLenght = 250
-  def jobURL = env.BUILD_URL
-
-  def reporterOptions = [
-    "--verbose",
-    "--env-description \"${testPlanDesc}\"",
-    "--testrail-run-update",
-    "--testrail-url \"${testrailURL}\"",
-    "--testrail-user \"\${TESTRAIL_USER}\"",
-    "--testrail-password \"\${TESTRAIL_PASSWORD}\"",
-    "--testrail-project \"${testrailProject}\"",
-    "--testrail-plan-name \"${testPlanName}\"",
-    "--testrail-milestone \"${testrailMilestone}\"",
-    "--testrail-suite \"${testSuiteName}\"",
-    "--xunit-name-template \"${methodname}\"",
-    "--testrail-name-template \"${testrail_name_template}\"",
-    "--test-results-link \"${jobURL}\"",
-    "--testrail-case-max-name-lenght ${testrailCaseMaxNameLenght}",
-  ] + reporter_extra_options
-
-  def script = """
-    . ${venvPath}/bin/activate
-    wget -O ${report_filename} $venvPath
-    set -ex
-    report ${reporterOptions.join(' ')} '${report_name}'
-  """
-
-  def testrail_cred_id = params.TESTRAIL_CRED ?: 'testrail_system_tests'
-
-  withCredentials([
-             [$class          : 'UsernamePasswordMultiBinding',
-             credentialsId   : testrail_cred_id,
-             passwordVariable: 'TESTRAIL_PASSWORD',
-             usernameVariable: 'TESTRAIL_USER']
-  ]) {
-    def ret = [:]
-    ret.stdout = ''
-    ret.exception = ''
-    try {
-        ret.stdout = sh(script: script, returnStdout: true)
-    } catch (Exception ex) {
-        ret.exception = ("""\
-##### Report to '${
-                         }' failed: #####\n""" + ex.message + "\n\n")
-    }
-    return ret
-  }
-}
+          ret = upload_results_to_testrail(report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
+          common.printMsg(ret.stdout, "blue")
+          report_url = ret.stdout.split("\n").each {
+            if (it.contains("[TestRun URL]")) {
+              common.printMsg("Found report URL: " + it.trim().split().last(), "blue")
+              description += "<a href=" + it.trim().split().last() + ">${testSuiteName}</a><br>"
+            }
+          } // report url
+        } // if val
+      } // iterate map
+  } //stage
+} //node
 
 def run_cmd(String cmd, Boolean returnStdout=false) {
     def common = new com.mirantis.mk.Common()
     common.printMsg("Run shell command:\n" + cmd, "blue")
-    def VENV_PATH='.venv-si-tests'
+    def VENV_PATH='/home/jenkins/fuel-devops30'
     def stderr_path = "/tmp/${JOB_NAME}_${BUILD_NUMBER}_stderr.log"
     def script = """#!/bin/bash
         set +x
@@ -166,4 +69,63 @@ def run_cmd(String cmd, Boolean returnStdout=false) {
     } finally {
         sh(script: "rm ${stderr_path} || true")
     }
+}
+
+def run_cmd_stdout(cmd) {
+    return run_cmd(cmd, true)
+}
+
+def upload_results_to_testrail(report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options=[]) {
+      def venvPath = '/home/jenkins/venv_testrail_reporter'
+      def testrailURL = "https://mirantis.testrail.com"
+      def testrailProject = "Mirantis Cloud Platform"
+      def testPlanNamePrefix = env.TEST_PLAN_NAME_PREFIX ?: "[KaaS]System"
+      def testPlanName = "${testPlanNamePrefix}-${ENV_NAME}-${new Date().format('yyyy-MM-dd')}"
+      def testPlanDesc = env.ENV_NAME
+      def testrailMilestone = "MCP2.0"
+      def testrailCaseMaxNameLenght = 250
+      def jobURL = env.BUILD_URL
+
+      def reporterOptions = [
+        "--verbose",
+        "--env-description \"${testPlanDesc}\"",
+        "--testrail-run-update",
+        "--testrail-url \"${testrailURL}\"",
+        "--testrail-user \"\${TESTRAIL_USER}\"",
+        "--testrail-password \"\${TESTRAIL_PASSWORD}\"",
+        "--testrail-project \"${testrailProject}\"",
+        "--testrail-plan-name \"${testPlanName}\"",
+        "--testrail-milestone \"${testrailMilestone}\"",
+        "--testrail-suite \"${testSuiteName}\"",
+        "--xunit-name-template \"${methodname}\"",
+        "--testrail-name-template \"${testrail_name_template}\"",
+        "--test-results-link \"${jobURL}\"",
+        "--testrail-case-max-name-lenght ${testrailCaseMaxNameLenght}",
+      ] + reporter_extra_options
+
+      def script = """
+        . ${venvPath}/bin/activate
+        set -ex
+        report ${reporterOptions.join(' ')} '${workspace}/${report_name}'
+      """
+
+      def testrail_cred_id = params.TESTRAIL_CRED ?: 'testrail_system_tests'
+
+      withCredentials([
+                 [$class          : 'UsernamePasswordMultiBinding',
+                 credentialsId   : testrail_cred_id,
+                 passwordVariable: 'TESTRAIL_PASSWORD',
+                 usernameVariable: 'TESTRAIL_USER']
+      ]) {
+        def ret = [:]
+        ret.stdout = ''
+        ret.exception = ''
+        try {
+            ret.stdout = run_cmd_stdout(script)
+        } catch (Exception ex) {
+            ret.exception = ("""\
+            ##### Report to failed: #####\n""" + ex.message + "\n\n")
+        }
+        return ret
+      }
 }
